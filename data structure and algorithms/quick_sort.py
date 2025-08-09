9f5fc694f5daa3a5cece03588940259940918526
juanaxyz <inijuana@gmail.com>
2025-08-09 15:21:54 +0800

    add file recursion and its implementation in quick sort algorithm

---
+++ data structure and algorithms/quick_sort.py
@@ -0,0 +1,39 @@
+import statistics
+import random 
+
+def get_random_numbers(n: int):
+    list_num = []
+    for _ in range(n):
+        list_num.append(random.randint(1,100))
+    return list_num
+
+def quick_sort(number_list: list):
+    smaller_list = []
+    greater_list = []
+    pivot_list = []
+    
+    if len(number_list) <=1:
+        return number_list
+    pivot = statistics.median(
+        [
+            number_list[0],
+            number_list[len(number_list) //2],
+            number_list[-1]
+        ]
+    )
+    
+    
+    for n in number_list:
+        if n < pivot:
+            smaller_list.append(n)
+        elif n == pivot:
+            pivot_list.append(n)
+        else:
+            greater_list.append(n)
+    
+    return (quick_sort(smaller_list) + pivot_list + quick_sort(greater_list))
+            
+    
+
+numbers = get_random_numbers(10)
+print(quick_sort(numbers))
---
+++ data structure and algorithms/recursion.py
@@ -0,0 +1,69 @@
+import time
+
+def countdown(n: int):
+    if n == 0:
+        print(f"boom!!!")
+        return
+        
+    print(f"you have {n} sec left")
+    time.sleep(1)
+    countdown(n-1)
+    
+    
+countdown(1)
+
+##########
+
+def factorial(n: int):
+    return 1 if n ==1 else n*factorial(n-1)
+    
+print(factorial(5))
+
+names = [
+    "Adam",
+    [
+        "Bob",
+        [
+            "Chet",
+            "Cat",
+        ],
+        "Barb",
+        "Bert"
+    ],
+    "Alex",
+    [
+        "Bea",
+        "Bill"
+    ],
+    "Ann"
+]
+
+def count_all(list_item: list):
+    count = 0
+    print(f"list = {list_item}")
+    for item in list_item:
+        if isinstance(item,list):
+            count += count_all(list_item=item)
+        else:
+            count +=1
+    return count
+
+print(count_all(names))
+
+
+# palindrom
+def check_palindrom(word: str):
+    return True if word[0] == word[::-1] else False
+
+def check_palindrom_recursive(word: str):
+    if len(word) <= 1:
+        return True
+    else:
+        return word[0] == word[-1] and check_palindrom_recursive(word[1:-1])
+    
+print(("foo")[::-1]) #::-1 itu slicing stepnya 1 dari belakang
+print(check_palindrom("foo"))
+
+words = input("masukkan kata : ")
+
+print(check_palindrom_recursive(words))
