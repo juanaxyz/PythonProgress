0b2e02ffaef28551028cd15f0ed6af991a9dcc12
juanaxyz <inijuana@gmail.com>
2025-08-10 19:08:27 +0800

    add file buble_sort.py implementation of sorting algorithm in python

---
+++ data structure and algorithms/buble_sort.py
@@ -0,0 +1,16 @@
+from random import randint
+
+def buble_sort(list_items: list):
+    for i in range(len(list_items)):
+        for j in range(len(list_items)-1):
+            if list_items[j] > list_items[j+1]:
+                temp = list_items[j]
+                list_items[j] = list_items[j+1]
+                list_items[j+1] = temp
+    print(list_items)
+
+numbers = []
+for n in range(10):
+    numbers.append(randint(1,100))
+        
+buble_sort(numbers)
